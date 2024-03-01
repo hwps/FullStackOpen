d825e39fffe799ce545a5544f96ea16ecc8240a7
Peter Schulman <614951+hwps@users.noreply.github.com>
2024-03-01 15:33:04 +0200

    feat: BlogList-frontend, show/hide additional info in Blog component, assignment 5.7

--- part5/BlogList-frontend/src/components/Blog.jsx
+++ part5/BlogList-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : ''}
+  const showWhenVisible = { display: visible ? '' : 'none'}
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+      {blog.author}: {blog.title}&nbsp;
+        <button onClick={() => toggleVisibility()}>{visible ? 'Hide' : 'Show'}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <a href={blog.url}>{blog.url}</a>
+        <div>
+          Likes: {blog.likes}&nbsp; <button>Like</button>
+        </div>
+        <div>
+          Added by: {blog.user.name}
+        </div>
+      </div> 
+    </div>
+  )
+}
 
 export default Blog
