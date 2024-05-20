7fe7f948a701387328b0e10d5d7e1fcedfff64f3
Peter Schulman <614951+hwps@users.noreply.github.com>
2024-05-20 13:02:08 +0300

    part6/redux-anecdotes: assignments 6.5 + 6.6

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,15 +2,21 @@ import { useSelector, useDispatch } from 'react-redux'
 import { upvoteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
+  // fetch anecdotes from store and sort according to likes
   const anecdotes = useSelector(state => state)
+  anecdotes.sort((a, b) => {
+    if (a.votes === b.votes) return 0
+    else return a.votes > b.votes ? -1 : 1
+  })
   const dispatch = useDispatch()
 
-  const vote = (id) => {
+  // 
+  const handleUpvote = (id) => {
     console.log('vote', id)
     dispatch(upvoteAnecdote(id))
   }
 
-  const newAnecdote = (event) => {
+  const handleNew = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
@@ -28,12 +34,12 @@ const App = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => handleUpvote(anecdote.id)}>vote</button>
           </div>
         </div>
       )}
       <h2>create new</h2>
-      <form onSubmit={newAnecdote}>
+      <form onSubmit={handleNew}>
         <div><input name="anecdote"/></div>
         <button type="submit">create</button>
       </form>
