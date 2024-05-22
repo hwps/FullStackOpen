812421a0530ce1104207369f0c26f846afebe229
Peter Schulman <614951+hwps@users.noreply.github.com>
2024-05-22 11:03:05 +0300

    part6/redux-anecdotes: anecdote filtering, assignment 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,8 +2,16 @@ import { useDispatch, useSelector } from "react-redux"
 import { upvoteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-    // fetch anecdotes from store and sort according to likes
-    const anecdotes = useSelector(state => state)
+    // fetch anecdotes from store matching filter
+    const anecdotes = useSelector(state => {
+        if (!state.filter) 
+            return state.anecdotes
+        else {
+            return state.anecdotes.filter(a => a.content.includes(state.filter))
+        }
+    })
+    
+    // sort according to likes
     anecdotes.sort((a, b) => {
         if (a.votes === b.votes) return 0
         else return a.votes > b.votes ? -1 : 1
